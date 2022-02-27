c3ab1270036f520c557cc690b18986920a8ae797
a-mackay <a-mackay@users.noreply.github.com>
2022-02-27 12:05:52 +1100

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/setupbgs">
         <React.StrictMode>
             <App />
         </React.StrictMode>
